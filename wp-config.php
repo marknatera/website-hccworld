866f3c3aaaca8b659eacb87955580c1b0ea1acd3
Mark Natera <marknatera@gmail.com>
2016-05-26 12:26:24 -0400

    WIP post to heroku

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hccworld');
+define('DB_NAME', 'heroku_f0c61b9fdf4c525');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'bf03091fc27929');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '776deb53');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
